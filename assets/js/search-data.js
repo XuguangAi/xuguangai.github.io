a36dafc643402307f4f7bdf4ef61991954b8fd85
XuguangAi <82428718+XuguangAi@users.noreply.github.com>
2025-11-13 04:19:30 +0000

    Deploying to gh-pages from @ XuguangAi/xuguangai.github.io@77be56cf8fe13b49e61fbc46c3c69c64380fdb4b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -518,13 +518,6 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/xuguang-ai-306375225", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
